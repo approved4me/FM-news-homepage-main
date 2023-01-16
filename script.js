cbd6ba6a623aa22ba3203a903a196e876d80df5b
approved4me <approved4me@gmail.com>
2023-01-16 15:43:31 +0100

    icon toggle

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ const mobileMenu = document.getElementById("nav");
 
 menuToggle.onclick = () => {
     mobileMenu.classList.toggle("active");
+    document.getElementsByClassName("icon")[0].classList.toggle("none");
+    document.getElementsByClassName("icon")[1].classList.toggle("none");
 }
